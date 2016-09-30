9c251189cbee43e1491b5930317303a9c772d51b
arr213 <arr213@gmail.com>
2016-09-30 15:49:25 -0400

    first commit

---
+++ server/db/models/SQLSummary.js
@@ -0,0 +1,67 @@
+// SQ Scheme Summary
+/* eslint-disable */ 
+
+User = {
+    email,
+    password,
+    salt,
+    twitter_id,
+    facebook_id,
+    google_id,
+}
+
+Game = {
+	users: [],
+	outcome: 'won' || 'lost',
+	modules: [mod1, mod],
+	timeAllotted: 1234,
+	timeUsed: 123,
+
+}
+
+
+squad1 = {
+	publicStatus: 'private' || 'public',
+	squadKey,
+	members: [user1, user2, ...],
+	modules: [mod1, mod2, ...],
+	timeLimit,
+	timeStarted,
+	timeCompleted,
+	chatRoom,
+	manual: [man1, man2],
+	bombQualities: [quality1, quality2, ...],
+	gameMode: 'standard' || 'simultaneous' || 'crazy hard mode'
+
+}
+
+//module object example
+mod1 = {
+	userAssigned: user1,
+	type: 'wires',
+	sequence: ['black', 'blue', 'red', 'white'],
+	solution: 3 //cut fourth wire
+	status: 'completed' || 'current' || 'pending',
+	timeStarted,
+	timeCompleted
+}
+
+//bomb quality object example
+quality1 = {
+	type: 'battery' || 'serial number' || 'DVI-port',
+	subtype, // color, 
+	userAssigned: user1
+}
+
+//user object example
+user1 = {
+	name: 'Rose',
+	readyStatus: 'ready' || 'not-ready'
+}
+
+//manual object example
+man1 = {
+	heading: 'wires',
+	body: TEXT,
+	usersAssigned: [user1, user2, ...]
+}
