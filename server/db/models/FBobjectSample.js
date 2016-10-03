78884a307ea308bd85b0eff144be3b070908062a
Stephanie Harris <310srh@gmail.com>
2016-10-03 15:05:03 -0400

    commented out files

--- server/db/models/FBobjectSample.js
+++ server/db/models/FBobjectSample.js
@@ -1,53 +1,53 @@
-/* eslint-disable */ 
-FBobj = {
-	games: [sq1, sq2, ...]
+// /* eslint-disable */
+// FBobj = {
+// 	games: [sq1, sq2, ...]
 
-}
+// }
 
-//squad object example
-game1 = {
-	publicStatus: 'private' || 'public',
-	squadKey,
-	members: [user1, user2, ...],
-	modules: [mod1, mod2, ...],
-	timeLimit,
-	timeStarted,
-	timeCompleted,
-	chatRoom,
-	manual: [man1, man2],
-	bombQualities: [quality1, quality2, ...],
-	gameMode: 'standard' || 'simultaneous' || 'crazy hard mode'
+// //squad object example
+// game1 = {
+// 	publicStatus: 'private' || 'public',
+// 	squadKey,
+// 	members: [user1, user2, ...],
+// 	modules: [mod1, mod2, ...],
+// 	timeLimit,
+// 	timeStarted,
+// 	timeCompleted,
+// 	chatRoom,
+// 	manual: [man1, man2],
+// 	bombQualities: [quality1, quality2, ...],
+// 	gameMode: 'standard' || 'simultaneous' || 'crazy hard mode'
 
-}
+// }
 
-//module object example
-mod1 = {
-	userAssigned: user1,
-	type: 'wires',
-	sequence: ['black', 'blue', 'red', 'white'],
-	solution: 3 //cut fourth wire
-	status: 'completed' || 'current' || 'pending',
-	timeStarted,
-	timeCompleted
-}
+// //module object example
+// mod1 = {
+// 	userAssigned: user1,
+// 	type: 'wires',
+// 	sequence: ['black', 'blue', 'red', 'white'],
+// 	solution: 3 //cut fourth wire
+// 	status: 'completed' || 'current' || 'pending',
+// 	timeStarted,
+// 	timeCompleted
+// }
 
-//bomb quality object example
-quality1 = {
-	type: 'battery' || 'serial number' || 'DVI-port',
-	subtype, // color, 
-	userAssigned: user1
-}
+// //bomb quality object example
+// quality1 = {
+// 	type: 'battery' || 'serial number' || 'DVI-port',
+// 	subtype, // color,
+// 	userAssigned: user1
+// }
 
-//user object example
-user1 = {
-	name: 'Rose',
-	readyStatus: 'ready' || 'not-ready'
-}
+// //user object example
+// user1 = {
+// 	name: 'Rose',
+// 	readyStatus: 'ready' || 'not-ready'
+// }
 
-//manual object example
-man1 = {
-	heading: 'wires',
-	body: TEXT,
-	usersAssigned: [user1, user2, ...]
-}
+// //manual object example
+// man1 = {
+// 	heading: 'wires',
+// 	body: TEXT,
+// 	usersAssigned: [user1, user2, ...]
+// }
 
