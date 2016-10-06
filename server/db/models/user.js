4c93e27f387b35faafd51560fccb330c9394578d
Rosana Liang <rosanajliang@gmail.com>
2016-10-06 16:55:41 -0400

    ready-start

--- server/db/models/user.js
+++ server/db/models/user.js
@@ -6,6 +6,9 @@ var Sequelize = require('sequelize');
 var db = require('../_db');
 
 module.exports = db.define('user', {
+    username: {
+        type: Sequelize.STRING
+    },
     email: {
         type: Sequelize.STRING
     },
