03d29511f4e29c98dc177f9cd34536a9896671f9
Rosana Liang <rosanajliang@gmail.com>
2016-10-06 17:12:17 -0400

    state goes to home and not game

--- browser/js/signup/signup.js
+++ browser/js/signup/signup.js
@@ -36,7 +36,7 @@ app.controller('SignupCtrl', function ($scope, $state, $log, AuthService, UserFa
                     AuthService.login($scope.loginInfo);
                 })
                 .then(function() {
-                    $state.go('game');
+                    $state.go('home');
                 })
                 .catch(function() {
                     $scope.error = 'Invalid sign up credentials.';
