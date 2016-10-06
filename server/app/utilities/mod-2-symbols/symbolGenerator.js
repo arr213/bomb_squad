7861bd737d628cd2d4cc756bc78273c9c71e74f8
arr213 <arr213@gmail.com>
2016-10-06 15:26:14 -0400

    symbolGen

--- server/app/utilities/mod-2-symbols/symbolGenerator.js
+++ server/app/utilities/mod-2-symbols/symbolGenerator.js
@@ -14,12 +14,18 @@ symbolGenerator.generate = function(game) {
 
 function buttonGen(column) {
     var thisCol = column;
-    _.times(3, function() {
-        thisCol.splice(_.random(0, thisCol.length), 1);
-    })
+    for (let i = 0; i < 3; i++) {
+        thisCol.splice(_.random(0, 6 - i), 1);
+    }
     for (let i = 0; i < thisCol.length; i++) {
         thisCol[i].pressOrder = i + 1;
         thisCol[i].pressed = false;
     }
+    thisCol = _.shuffle(thisCol);
     return thisCol;
 }
+
+// // Test below:
+// var testObj = {};
+// symbolGenerator.generate(testObj);
+// console.log(testObj)
