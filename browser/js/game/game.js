aac75f80eb62c6a3f62872672a2a1797518f8842
Naohito Sadoshima <nsadoshima@gmail.com>
2016-10-08 23:42:36 -0400

    broke wires?

--- browser/js/game/game.js
+++ browser/js/game/game.js
@@ -9,18 +9,17 @@ app.config(function ($stateProvider) {
 });
 
 app.controller('GameCtrl', function ($scope, $stateParams, $state) {
-    // console.log('Setting the currentStage now.');
+
+    let interval;
     $scope.currentStage = 0;
     var rootRef = firebase.database().ref('/game');
     $scope.currentGame = rootRef.child($stateParams.gameKey);
-
 
     $scope.currentGame.child('stages').once('value', function (snap) {
         $scope.stages = snap.val();
         $scope.currentModule = $scope.stages[$scope.currentStage].modules[0];
         $scope.$evalAsync();
     });
-
 
     $scope.currentGame.child('currentStage').on('value', function (snap) {
         $scope.currentStage = snap.val();
@@ -29,33 +28,11 @@ app.controller('GameCtrl', function ($scope, $stateParams, $state) {
             clearInterval(interval);
             $state.go('victory');
         }
-        // console.log('the currentStage has been set to: ', $scope.currentStage);
     });
-
-    // console.log($scope.currentModule);
-
-    // $scope.loggedInUserId = 1;
-
-    // $scope.components = [{
-    //     userAssigned: 1,
-    //     type: 'battery',
-    //     content: null
-    // }, {
-    //     userAssigned: 2,
-    //     type: 'serial number',
-    //     content: '1238548'
-    // }, {
-    //     userAssigned: 3,
-    //     type: 'battery',
-    //     content: null
-    // }];
-
-    let interval;
-    // $scope.squadName = 'the squad';
-
-    // console.log('plz work', $scope.error)
-    $scope.currentGame.on('value', function (snapshot) {
-        $scope.strikes = snapshot.val().strikes;
+    
+    $scope.currentGame.child('strikes').on('value', function (snapshot) {
+        $scope.strikes = snapshot.val();
+        console.log($scope.strikes);
         if ($scope.strikes[2]['active']) {
             $scope.currentGame = null;
             clearInterval(interval);
@@ -89,14 +66,11 @@ app.controller('GameCtrl', function ($scope, $stateParams, $state) {
                 startTime: Date.now()
             });
         }
-
         $scope.timerNum = '';
-
         $scope.currentGame.on('value', function (snapshot) {
             $scope.startTime = snapshot.val().startTime;
             $scope.timeLimit = snapshot.val().timeLimit;
         });
-
         var interval = setInterval(function () {
             if ($scope.timerNum === '0:00') {
                 console.log('YOU EXPLODED!!!!');
@@ -104,7 +78,6 @@ app.controller('GameCtrl', function ($scope, $stateParams, $state) {
                 $state.go('gameover');
                 return;
             }
-
             var ms = $scope.timeLimit - (Date.now() - $scope.startTime);
             var seconds = function () {
                 var s = (ms / 1000) % 60;
@@ -113,14 +86,11 @@ app.controller('GameCtrl', function ($scope, $stateParams, $state) {
                 } else {
                     return s.toString().slice(0, 2);
                 }
-
             }
-
             $scope.timerNum = (((ms / 1000 / 60) << 0) + ':' + seconds());
             $scope.$evalAsync();
         }, 500);
-
-    }
+    };
 
 
 });
