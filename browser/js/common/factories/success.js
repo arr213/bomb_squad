e248d7fc3d16867b440160274b82368c732a7143
Naohito Sadoshima <nsadoshima@gmail.com>
2016-10-08 21:33:11 -0400

    victory trigger improved

--- browser/js/common/factories/success.js
+++ browser/js/common/factories/success.js
@@ -3,9 +3,8 @@ app.factory('SuccessFactory', function(){
     const SuccessFactory = {}
 
     SuccessFactory.success = function(currentStage,game){
+        currentStage++;
         var curStage = angular.copy(currentStage);
-        console.log(curStage);
-        curStage++;
         game.update({currentStage: curStage});
     }
 
