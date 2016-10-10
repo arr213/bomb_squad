599bd7ce9619f69fdf25e385f5a947594d2931a7
Naohito Sadoshima <nsadoshima@gmail.com>
2016-10-10 12:48:52 -0400

    added angular mobile

--- browser/js/home/home.js
+++ browser/js/home/home.js
@@ -1,8 +1,31 @@
 app.config(function ($stateProvider) {
     $stateProvider.state('home', {
         url: '/home',
-        templateUrl: 'js/home/home.html'
+        templateUrl: 'js/home/home.html',
+        controller: 'HomeCtrl'
     });
 });
+
+app.controller('HomeCtrl', function($scope){
+
+    
+
+    $scope.swiped = false;
+
+    $scope.onSwipeRight = function(ev){
+        console.log('swiped right!')
+        if(!$scope.swiped){
+            $scope.swiped = true;
+        }
+    };
+
+    $scope.onSwipeLeft = function(ev){
+        console.log('swiped left!');
+        if($scope.swiped){
+            $scope.swiped = false;
+        }
+    };
+
+})
 
 
