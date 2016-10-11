0c7544c6fec5629358e36982a8eca500942d914f
Stephanie Harris <310srh@gmail.com>
2016-10-11 17:48:28 -0400

    fixed symbols, and cosmetic changes to init pgs

--- browser/js/common/directives/symbols/symbols.js
+++ browser/js/common/directives/symbols/symbols.js
@@ -11,34 +11,35 @@ app.directive('symbols', function () {
 
 });
 
-app.controller('SymbolsCtrl', function ($scope, StrikeFactory, SuccessFactory, $stateParams) {
+app.controller('SymbolsCtrl', function($scope, StrikeFactory, SuccessFactory, $stateParams) {
 
-  let gameRef = firebase.database().ref('/game').child($stateParams.gameKey);
+    let gameRef = firebase.database().ref('/game').child($stateParams.gameKey);
 
-  gameRef.on('value', function (snap) {
+    gameRef.on('value', function(snap) {
         $scope.currentGame = snap.val();
         $scope.strikes = $scope.currentGame.strikes;
         $scope.currentStage = snap.val().currentStage;
         $scope.$evalAsync();
     });
 
-  $scope.symbols = $scope.module.content;
+    $scope.symbols = $scope.module.content;
 
-  $scope.buttonPress = function (symbol) {
-    if (!symbol.pressed) {
-      if (symbol.pressOrder === $scope.correctPressCount) {
-        $scope.correctPressCount++;
-        symbol.pressed = true;
-        if($scope.correctPressCount===5){
-          SuccessFactory.success($scope.currentStage, gameRef);
+    $scope.buttonPress = function(symbol) {
+        if (!symbol.pressed) {
+            if (symbol.pressOrder === $scope.correctPressCount) {
+                $scope.correctPressCount++;
+                symbol.pressed = true;
+                if ($scope.correctPressCount === 5) {
+                    SuccessFactory.success($scope.currentStage, gameRef);
+                }
+            } else {
+                symbol.wrong = true;
+                StrikeFactory.strike($scope.strikes, gameRef);
+            }
         }
-      } else {
-        StrikeFactory.strike($scope.strikes, gameRef);
-      }
-    }
-  };
+    };
 
-  $scope.correctPressCount = 1;
-
+    $scope.correctPressCount = 1;
 
 });
+
