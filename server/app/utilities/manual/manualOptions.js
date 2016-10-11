1cb365f564fae008f5fe8d9b43e096c9300985f9
arr213 <arr213@gmail.com>
2016-10-11 15:48:25 -0400

    manual directives

--- server/app/utilities/manual/manualOptions.js
+++ server/app/utilities/manual/manualOptions.js
@@ -4,15 +4,12 @@ module.exports = manualOptions;
 
 manualOptions.intro = {
 	moduleType: 'intro',
-	usersAssigned: [],
 }
 
 manualOptions.wires = {
     moduleType: 'wires',
-    usersAssigned: [],
 };
 
 manualOptions.symbols = {
     moduleType: 'symbols',
-    usersAssigned: [],
 };
