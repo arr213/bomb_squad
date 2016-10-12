b49561862c4fd68db102b9b92504d02dae4d886f
arr213 <arr213@gmail.com>
2016-10-11 20:44:53 -0400

    manuel

--- server/app/utilities/manual/manualGenerator.js
+++ server/app/utilities/manual/manualGenerator.js
@@ -5,7 +5,7 @@ const manualOptions = require('./manualOptions');
 
 
 manualGenerator.generate = function(game, userAssigned) {
-    console.log('Removing user: ', userAssigned);
+    // console.log('Removing user: ', userAssigned);
     let manual = _.cloneDeep(manualOptions);
     let usersAssigned = _.cloneDeep(game.users);
     usersAssigned.splice(usersAssigned.indexOf(userAssigned), 1);
