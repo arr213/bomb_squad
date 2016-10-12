f5e187ae8a7d60015ba682f7653afebc878b11cb
Naohito Sadoshima <nsadoshima@gmail.com>
2016-10-12 01:06:04 -0400

    merged and replaced symbol 15

--- browser/js/game/game.js
+++ browser/js/game/game.js
@@ -43,18 +43,12 @@ app.controller('GameCtrl', function ($scope, $stateParams, $state, $rootScope, $
 
     $scope.currentGame.child('batteries').once('value', function (snap) {
         $scope.batteries = snap.val();
-        console.log('batteriesssss', $scope.batteries)
-        $scope.userId = $rootScope.userId;
-        // $scope.user = AuthService.getLoggedInUser()
-             console.log('userrr', $scope.userId)
-        // $scope.loggedInUserId = user.id;
-        // $scope.batteries = [];
-        // console.log('@@@@@', batteryObject)
-        // for(var i = 0; i < batteryArray.length; i++){
-        //     if(user.id === batteryArray[i].userAssigned){
-        //         $scope.batteries.push(batteryArray[i]);
-        //     }
+        for(var i=0; i<$scope.batteries.length;i++){
+            console.log('batteriesssss color', $scope.batteries[i].color)
+        }
 
+
+        $scope.userId = $rootScope.userId;
     })
 
     $scope.currentGame.once('value', function (snap) {
--- server/app/utilities/mod-2-symbols/symbolOptions.js
+++ server/app/utilities/mod-2-symbols/symbolOptions.js
@@ -61,8 +61,8 @@ symbolOptions.symbolList = [{
     unicode: 'Ж'
 }, {
     id: 15,
-    name: 'right-half-R',
-    unicode: 'Ꝛ'
+    name: 'swirly',
+    unicode: '₪'
 }, {
     id: 16,
     name: 'lowercase-delta',
