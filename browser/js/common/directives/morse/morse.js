7f21bdacdd02518bdaaf3dc56ff489a2e51b4932
arr213 <arr213@gmail.com>
2016-10-15 15:09:59 -0400

    morse numbers

--- browser/js/common/directives/morse/morse.js
+++ browser/js/common/directives/morse/morse.js
@@ -25,19 +25,22 @@ app.controller('MorseCtrl', function($scope, StrikeFactory, $http, $stateParams,
     var wordObj = $scope.module.content;
     $scope.word = wordObj.word;
     $scope.solutionFrequency = wordObj.frequency;
+    $scope.frequencyOptions = [505, 515, 522, 532, 535, 542, 545, 552, 555, 565, 572, 575, 582, 592, 595, 600];
 
-    $scope.frequency = 500;
+    $scope.frequencyIndex = 0;
 
     $scope.increaseFrequency = function() {
-        $scope.frequency++;
+        if ($scope.frequencyIndex === $scope.frequencyOptions.length - 1) $scope.frequencyIndex = 0;
+        else $scope.frequencyIndex++;
     };
 
     $scope.decreaseFrequency = function() {
-        $scope.frequency--;
+        if ($scope.frequencyIndex === 0) $scope.frequencyIndex = $scope.frequencyOptions.length - 1;
+        else $scope.frequencyIndex--;
     };
 
     $scope.submit = function() {
-        if ('3.' + $scope.frequency === $scope.solutionFrequency) {
+        if ('3.' + $scope.frequencyOptions[$scope.frequencyIndex] === $scope.solutionFrequency) {
             SuccessFactory.success($scope.currentStage, gameRef);
         } else {
             StrikeFactory.strike($scope.strikes, gameRef);
