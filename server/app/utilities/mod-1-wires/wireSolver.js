d4c9c73bf91b8148db582c9f8591787110e393c0
Naohito Sadoshima <nsadoshima@gmail.com>
2016-10-15 17:11:56 -0400

    fixed rules part deux

--- server/app/utilities/mod-1-wires/wireSolver.js
+++ server/app/utilities/mod-1-wires/wireSolver.js
@@ -11,19 +11,19 @@ wireSolver.solve = function(arr, batteries) {
 
 //defining rules of what wires to cut
 function threeWireSolver(arr, batteries) {
-    if (_.filter(batteries, _.matches({ color: 'blue' }).length % 2 === 0) && _.filter(batteries, _.matches({ color: 'yellow' }).length % 2 !== 0)) return 0;
+    if (_.filter(batteries, _.matches({ color: 'blue' }).length % 2 === 0) && _.filter(batteries, _.matches({ color: 'yellow' })).length % 2 !== 0) return 0;
     if (arr.indexOf('red') === -1) return 1;
     if (arr[2] === 'white') return 2;
-    if (arr[0] === 'black' && _.filter(batteries, _.matches({ color: 'blue' }).length % 2 === 0)) return 0;
+    if (arr[0] === 'black' && _.filter(batteries, _.matches({ color: 'blue' })).length % 2 === 0) return 0;
     if (arr.filter(color => color === 'blue').length > 1) return arr.lastIndexOf('blue');
     return 2;
 }
 
 function fourWireSolver(arr, batteries) {
-    if ((arr.filter(color => color === 'red').length > 1) && _.filter(batteries, _.matches({ color: 'green' }).length % 2 !== 0)) return arr.lastIndexOf('red');
+    if ((arr.filter(color => color === 'red').length > 1) && _.filter(batteries, _.matches({ color: 'green' })).length % 2 !== 0) return arr.lastIndexOf('red');
     if (arr[3] === 'yellow' && arr.indexOf('red') === -1) return 0;
     if (arr.filter(color => color === 'blue').length === 1) return 0;
-    if (arr.indexOf('red') === -1 && _.filter(batteries, _.matches({ color: 'blue' }).length % 2 !== 0)) return 2;
+    if (arr.indexOf('red') === -1 && _.filter(batteries, _.matches({ color: 'blue' })).length % 2 !== 0) return 2;
     if (arr.filter(color => color === 'yellow').length > 1) return 3;
     return 1;
 }
@@ -31,16 +31,16 @@ function fourWireSolver(arr, batteries) {
 function fiveWireSolver(arr, batteries) {
     if (arr[4] === 'black' && _.filter(batteries, _.matches({ color: 'yellow' }).length % 2 !== 0)) return 3;
     if (arr.filter(color => color === 'red').length === 1 && arr.filter(color => color === 'yellow').length > 1) return 0;
-    if (arr.filter(color => color === 'white').length > 1 && _.filter(batteries, _.matches({ color: 'blue' }).length % 2 !== 0)) return 1;
+    if (arr.filter(color => color === 'white').length > 1 && _.filter(batteries, _.matches({ color: 'blue' })).length % 2 !== 0) return 1;
     if (arr.indexOf('black') === -1)  return 1;
     return 0;
 }
 
 function sixWireSolver(arr, batteries) {
     if (arr.indexOf('yellow') === -1 && _.filter(batteries, _.matches({ color: 'blue' }).length % 2 !== 0)) return 2;
-    if (_.filter(batteries, _.matches({ color: 'yellow' }).length % 2 !== 0) && _.filter(batteries, _.matches({ color: 'green' }).length % 2 !== 0)) return 0;
+    if (_.filter(batteries, _.matches({ color: 'yellow' }).length % 2 !== 0) && _.filter(batteries, _.matches({ color: 'green' })).length % 2 !== 0) return 0;
     if (arr.filter(color => color === 'yellow').length === 1 && arr.filter(color => color === 'white').length > 1) return 3;
-    if (arr.filter(color => color === 'blue').length > 1 && _.filter(batteries, _.matches({ color: 'green' }).length % 2 === 0)) return 4;
+    if (arr.filter(color => color === 'blue').length > 1 && _.filter(batteries, _.matches({ color: 'green' })).length % 2 === 0) return 4;
     if (arr.indexOf('red') === -1) return 5;
     return 3;
 }
