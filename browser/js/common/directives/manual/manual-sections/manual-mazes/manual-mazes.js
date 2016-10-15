8b8471b99aa904dd5195db2118a79c87f26ec033
Naohito Sadoshima <nsadoshima@gmail.com>
2016-10-15 13:17:31 -0400

    working Manuals

---
+++ browser/js/common/directives/manual/manual-sections/manual-mazes/manual-mazes.js
@@ -0,0 +1,10 @@
+// manual-intro.js
+app.directive('manualMazes', function() {
+
+    return {
+        restrict: 'E',
+        templateUrl: 'js/common/directives/manual/manual-sections/manual-mazes/manual-mazes.html',
+        // controller: 'ManualmazeController'
+    };
+
+});
