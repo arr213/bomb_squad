2f7d52181794475ff7d716df9cc8bcf7e3015f01
Stephanie Harris <310srh@gmail.com>
2016-10-15 18:36:18 -0400

    passwords module complete

--- browser/js/common/directives/passwords/passwords.js
+++ browser/js/common/directives/passwords/passwords.js
@@ -1,59 +1,55 @@
-// app.directive('passwords', function() {
+app.directive('passwords', function() {
 
-//     return {
-//         restrict: 'E',
-//         templateUrl: 'js/common/directives/passwords/passwords.html',
-//         scope: {
-//             module: '='
-//         },
-//         controller: 'PasswordsCtrl'
-//     };
-
-// });
-
-app.config(function ($stateProvider) {
-    $stateProvider.state('passwords', {
-        url: '/passwords',
+    return {
+        restrict: 'E',
         templateUrl: 'js/common/directives/passwords/passwords.html',
-        controller: 'PasswordsCtrl',
-    });
+        scope: {
+            module: '='
+        },
+        controller: 'PasswordsCtrl'
+    };
+
 });
+
 
 app.controller('PasswordsCtrl', function($scope, StrikeFactory, $http, $stateParams, $firebaseObject, $firebaseArray, SuccessFactory) {
 
-    // let gameRef = firebase.database().ref('/game').child($stateParams.gameKey);
+    let gameRef = firebase.database().ref('/game').child($stateParams.gameKey);
 
-    // gameRef.on('value', function(snap) {
-    //     $scope.currentGame = snap.val();
-    //     $scope.strikes = $scope.currentGame.strikes;
-    //     $scope.currentStage = snap.val().currentStage;
-    //     $scope.$evalAsync();
-    // });
+    gameRef.on('value', function(snap) {
+        $scope.currentGame = snap.val();
+        $scope.strikes = $scope.currentGame.strikes;
+        $scope.currentStage = snap.val().currentStage;
+        $scope.$evalAsync();
+    });
 
-    // var passwordObj = $scope.module.content;
-    // $scope.password = passwordObj.word;
-    // $scope.combinationArrays = passwordObj.shuffledPassword;
-
-    $scope.password = 'about';
-    $scope.combinationArrays = [
-    ['a', 'e', 'c', 'y', 'm'],
-    ['b', 'l', 'z', 'k', 'f'],
-    ['o', 'p', 't', 'i', 'r'],
-    ['u', 'g', 'q', 's', 'c'],
-    ['t', 'g', 'i', 'o', 'p']
-    ];
+    $scope.password = $scope.module.content;
+    $scope.columnCombos = $scope.password.combinations;
 
 
-    $scope.firstColumn = $scope.combinationArrays[0];
-    $scope.secondColumn = $scope.combinationArrays[1];
-    $scope.thirdColumn = $scope.combinationArrays[2];
-    $scope.fourthColumn = $scope.combinationArrays[3];
-    $scope.fifthColumn = $scope.combinationArrays[4];
+    $scope.goUp = function(column) {
+        if (column.currentIndex === column.letterOptions.length - 1) {
+            column.currentIndex = 0;
+        } else {
+            column.currentIndex++;
+        }
+    }
 
-    console.log('passwordObj: ', passwordObj);
+
+    $scope.goDown = function(column) {
+        if (column.currentIndex === 0) {
+            column.currentIndex = column.letterOptions.length - 1;
+        } else {
+            column.currentIndex--;
+        }
+    }
 
     $scope.submit = function() {
-        if ('3.' + $scope.frequency === $scope.solutionFrequency) {
+        let guess = '';
+        $scope.columnCombos.forEach(function(columnObj){
+            guess += columnObj.letterOptions[columnObj.currentIndex]
+        })
+        if (guess === $scope.password.word) {
             SuccessFactory.success($scope.currentStage, gameRef);
         } else {
             StrikeFactory.strike($scope.strikes, gameRef);
@@ -62,4 +58,3 @@ app.controller('PasswordsCtrl', function($scope, StrikeFactory, $http, $statePar
     };
 
 });
-
