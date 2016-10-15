19151d244db6145cf4a60029d08620511d99fa8a
Stephanie Harris <310srh@gmail.com>
2016-10-15 14:08:11 -0400

    module started

---
+++ server/app/utilities/mod-5-passwords/passwordOptions.js
@@ -0,0 +1,22 @@
+const passwordOptions = {};
+module.exports = passwordOptions;
+
+passwordOptions.combinationList = [{
+    word: 'about',
+    combinations: [
+    ['a', 'e', 'c', 'y', 'm'],
+    ['b', 'l', 'z', 'k', 'f'],
+    ['o', 'p', 't', 'i', 'r'],
+    ['u', 'g', 'q', 's', 'c'],
+    ['t', 'g', 'i', 'o', 'p']
+    ]
+}, {
+    word: 'study',
+    combinations: [
+    ['s', 'o', 'a', 'n', 't'],
+    ['t', 'g', 'y', 'e', 'r'],
+    ['u', 'l', 'e', 'c', 'h'],
+    ['d', 'l', 'w', 'f', 'z'],
+    ['y', 'm', 'o', 'q', 'i']
+    ]
+}]
