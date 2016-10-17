dc1c494bd33a68b0df1cc60b9b2a1b64d1555ca3
Rosana Liang <rosanajliang@gmail.com>
2016-10-17 10:24:35 -0400

    stuff

--- server/app/utilities/module/moduleGenerator.js
+++ server/app/utilities/module/moduleGenerator.js
@@ -9,20 +9,20 @@ const morseGenerator = require('../mod-4-morse/morseGenerator');
 const passwordGenerator = require('../mod-5-passwords/passwordGenerator');
 
 const modTypes = [{
-    type: 'wires',
-    generate: wireGenerator.generate
-}, {
-    type: 'symbols',
-    generate: symbolGenerator.generate
-}, {
-    type: 'maze',
-    generate: mazeGenerator.generate
-}, {
+//     type: 'wires',
+//     generate: wireGenerator.generate
+// }, {
+    // type: 'symbols',
+    // generate: symbolGenerator.generate
+// }, {
+    // type: 'maze',
+    // generate: mazeGenerator.generate
+// }, {
     type: 'morse',
     generate: morseGenerator.generate
-}, {
-    type: 'passwords',
-    generate: passwordGenerator.generate
+// }, {
+//     type: 'passwords',
+//     generate: passwordGenerator.generate
 }];
 
 
