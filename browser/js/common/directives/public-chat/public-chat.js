7cc86c8bd26fce76cd2f44a294cd4ed0d69aa2d3
Stephanie Harris <310srh@gmail.com>
2016-10-17 13:04:29 -0400

    chat working in-game, lobby, and staging area

--- browser/js/common/directives/public-chat/public-chat.js
+++ browser/js/common/directives/public-chat/public-chat.js
@@ -36,6 +36,7 @@ app.controller('PublicChatCtrl', function($scope, AuthService) {
     chatRef.on('child_added', function(snap) {
         $scope.updateScroll();
         $scope.messages.push(snap.val())
+        $scope.evalAsync();
     })
 
 
